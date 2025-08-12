e4ecf04a54e912dbc0473217f349a945af78f1c7
GedeNanda <igedeanandabelapersada2476@gmail.com>
2025-08-12 19:55:02 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteGhPages } from "vite-plugin-gh-pages";
 
 export default defineConfig({
-  plugins: [react(), viteGhPages()],
-  base: "/search-pokemon/", // ganti dengan nama repo GitHub Anda
+  plugins: [react()],
+  base: "/search-pokemon/",
 });
